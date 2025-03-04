6c2f8979520b9d727f7e86d074fd8854cc6b6583
hyeseon0105 <hyesun0536@gmail.com>
2025-03-04 09:44:29 +0900

    #5.API TEST-2

--- src/main/java/com/my/articles/controller/ApiTestController.java
+++ src/main/java/com/my/articles/controller/ApiTestController.java
@@ -4,10 +4,11 @@ import com.my.articles.dto.LoginDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ApiTestController {
@@ -16,14 +17,53 @@ public class ApiTestController {
         return "/test/api";
     }
 
-//    @RequestBody:json형식을>Object(dto)
+    //    @RequestBody:json형식을>Object(dto)
 //    리턴 타입이 :ResponseEntity< Object전달
     @PostMapping("/apiPostTest")
-    public ResponseEntity<String> apiPostTest(@RequestBody LoginDTO dto){
-        String info = dto.getUserid()+"/"+dto.getPassword();
+    public ResponseEntity<String> apiPostTest(@RequestBody LoginDTO dto) {
+        String info = dto.getUserid() + "/" + dto.getPassword();
         System.out.println("===== info :" + info);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(info);
+    }
+
+    @PostMapping("/getResponse")
+    @ResponseBody
+    public LoginDTO postResponse() {
+        return new LoginDTO("안유진", "1233");
+    }
+
+    @PostMapping("apiPostArrayTest")
+    public ResponseEntity<Map<String, String>> apiPostArraryTest(
+            @RequestBody List<LoginDTO> dtos
+    ) {
+        dtos.stream().forEach(x -> System.out.println(x));
+        Map<String, String> userData = new HashMap<>();
+        userData.put("id", dtos.get(1).getUserid());
+        userData.put("pw", dtos.get(1).getPassword());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userData);
+    }
+
+    @DeleteMapping("apiTest")
+    @ResponseBody
+    public String apiHttpMethodTest() {
+        String message = "Delete Mapping";
+        return message;
+    }
+
+    @PatchMapping("apiTest")
+    @ResponseBody
+    public String apiPatchTest() {
+        String message = "Patch Mapping";
+        return message;
+    }
+    @PutMapping("apiTest")
+    @ResponseBody
+    public String apiPutTest() {
+        String message = "put Mapping";
+        return message;
     }
 }
